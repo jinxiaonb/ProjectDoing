5cd571777018cba34e6a728c810e8198da10fbf1
jinxiaonb <315232021@163.com>
2016-09-08 10:01:34 +0800

    add a demo

--- vueTest/src/main.js
+++ vueTest/src/main.js
@@ -3,6 +3,7 @@ import Vue from "../node_modules/vue/dist/vue.min.js";
 import app from './components/app';
 import login from './components/login';
 import test from './components/test';
+import demo from './components/demo';
 
 Vue.config.debug = true;
 
@@ -11,3 +12,5 @@ new Vue(app);
 new Vue(login);
 
 new Vue(test);
+
+new Vue(demo);
